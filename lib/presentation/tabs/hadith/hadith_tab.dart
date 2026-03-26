8772324563da4bad1f3b27aa5df2fe5f5717788d
menna <mennaa.wahed@gmail.com>
2026-03-26 03:15:24 +0200

    +finished hadith tab

--- lib/presentation/tabs/hadith/hadith_tab.dart
+++ lib/presentation/tabs/hadith/hadith_tab.dart
@@ -1,10 +1,26 @@
+import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
+import 'package:islame_app/presentation/tabs/hadith/widget/hadeth_card.dart';
 
 class HadithTab extends StatelessWidget {
-  const HadithTab({super.key});
-
+  HadithTab({super.key});
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    var height = MediaQuery
+        .of(context)
+        .size
+        .height;
+    return CarouselSlider(
+      options: CarouselOptions(
+          height: height * 0.66,
+          enlargeCenterPage: true,
+          initialPage: 0
+      ),
+      items: List.generate(50, (index) => index + 1,).map((index) {
+        return HadethCard(index: index,);
+      }).toList(),
+    );
   }
+
+
 }
